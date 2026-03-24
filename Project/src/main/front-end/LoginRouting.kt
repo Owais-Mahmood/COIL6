2f2664895cb02048133849d1b78d6d1cc721385a
LexiJaryy <mbdz8562@leeds.ac.uk>
2026-03-24 16:04:37 +0000

    routingImagespt2

--- Project/src/main/front-end/LoginRouting.kt
+++ Project/src/main/front-end/LoginRouting.kt
@@ -1,7 +1,9 @@
-
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    
+
     // routing logic to resolve image linking issues
     routing { 
         staticResources("/","front-end")
