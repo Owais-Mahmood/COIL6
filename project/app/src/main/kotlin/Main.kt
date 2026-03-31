588969741827b62316e74d7b2162c51ae41aad9a
Owais-Mahmood <jpdj5670@leeds.ac.uk>
2026-03-31 13:35:40 +0000

    Adding Alerts Function for Web Browser

--- project/app/src/main/kotlin/Main.kt
+++ project/app/src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import backend.getAlertReadings
+import backend.getReadingsForSite
+import backend.loadWaterQualityData
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.response.respondText
@@ -5,10 +8,27 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
 fun main() {
+    val filePath = "../../datasets/datasets/synthetic_outputs/water_quality.csv"
+    val waterReadings = loadWaterQualityData(filePath)
+
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         routing {
+
+            // Home
             get("/") {
                 call.respondText("Hello, World!")
+            }
+
+            // Alerts
+            get("/alerts") {
+                val alertReadings = getAlertReadings(waterReadings)
+                call.respondText("Total alert-triggered readings: ${alertReadings.size}")
+            }
+
+            // Site-specific (hardcoded for now)
+            get("/site/site_upstream") {
+                val siteReadings = getReadingsForSite(waterReadings, "site_upstream")
+                call.respondText("Total readings for site_upstream: ${siteReadings.size}")
             }
         }
     }.start(wait = true)
