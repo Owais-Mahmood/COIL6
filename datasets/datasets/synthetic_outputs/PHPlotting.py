cff3707e9721fb9cde0a5a9f9d83fe9a62a29ec0
LexiJaryy <mbdz8562@leeds.ac.uk>
2026-04-10 16:31:10 +0000

    PH ADDITIONS3

--- datasets/datasets/synthetic_outputs/PHPlotting.py
+++ datasets/datasets/synthetic_outputs/PHPlotting.py
@@ -11,16 +11,17 @@ import sys # for reading kotlin code
 # create infographic
 fig,axes = plt.subplots()
 
+# circle 
+infographicCircle = plt.Circle((0.5,0.5),0.5, color = "#225382") # stroomloop blue
+axes.add_patch(infographicCircle)
 
 # ring
 green = "#008000"
 orange = "#FFA500"
 red = "#FF0000"
 ringColour = green # can insert logic of how to pick the colour later
-ring = Wedge(center=(0.5,0.5),r=0.25,theta1 = 0, theta2 = 90, width = 1, facecolor = ringColour)
-
-# circle 
-infographicCircle = plt.Circle((0.5,0.5),0.5, color = "#225382") # stroomloop blue
+ring = Wedge(center=(0.5,0.5),r=0.25,theta1 = 0, theta2 = 90, width = 0.1, facecolor = ringColour)
+axes.add_patch(ring)
 
 # readings in the centre of the circle
 axes.text(0.5, 0.5,"LIVE READING", ha = 'center', va = 'center', fontsize = 15, color = 'white')
@@ -36,7 +37,12 @@ axes.text(0.5,0.25,status,ha = 'center', va = 'center', fontsize = 13, color = '
 # timestamp
 ####### insert code
 
+# organising data
 axes.set_aspect(1) # so circle doesnt become an oval
-axes.add_patch(infographicCircle) # circle not drawn until attached to axis
+axes.set_xlim(0,1) # so ring is shown
+axes.set_ylim(0,1) # so ring is shown
+axes.axis("off")
+
+# create image
 plt.savefig("Infographiccircle.png")
 plt.show()
