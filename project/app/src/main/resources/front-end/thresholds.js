c79a20e54821ea8f3320b1523f95ebb2f5287b2a
Owais-Mahmood <jpdj5670@leeds.ac.uk>
2026-05-08 15:48:36 +0000

    Finalising Edits

--- project/app/src/main/resources/front-end/thresholds.js
+++ project/app/src/main/resources/front-end/thresholds.js
@@ -3,6 +3,6 @@ const THRESHOLDS = {
     turbidity:    { warn: 5,   crit: 10   },
     conductivity: { warn: 500, crit: 1500 },
     waterTemp:    { warnMin: 10, warnMax: 25, critMin: 5,  critMax: 30  },
-    waterLevel: { warnMin: 50, warnMax: 600, critMin: 20, critMax: 700 },
+    waterLevel: { warnMin: 30, warnMax: 750, critMin: 10, critMax: 800 },
     light:        { warn: 15000, crit: 100000 }
 };
